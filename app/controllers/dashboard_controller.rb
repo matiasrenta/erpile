8b5ce7d4ee51286d2ecd5d59ffeee7d3cd4e897b
Matias Renta <matiasrenta@gmail.com>
2020-09-21 21:02:36 -0300

    dashboard caja

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,12 @@ class DashboardController < ApplicationController
   def index
     authorize! :read, :dashboard
     set_content_title('fa-fw fa fa-dashboard', ['Dashboard'])
+
+    @caja_maxi = User.find_by_email('maximiliano@lapile.com.ar').caja
+    @caja_javi = User.find_by_email('javier@lapile.com.ar').caja
+    @caja_marcos = User.find_by_email('marcos@lapile.com.ar').caja
+    @caja_mati = User.find_by_email('matias@lapile.com.ar').caja
+    @caja_lapile = @caja_maxi + @caja_javi + @caja_marcos + @caja_mati
   end
 
   private
